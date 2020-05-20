cfa870c4bead52ccf76343d2096047360b467f02
Jens L. S <jens.l.s@live.dk>
2020-05-20 12:05:48 +0200

    Update check_credentials.js
    Fitting coding standards.

--- Server_files/check_credentials.js
+++ Server_files/check_credentials.js
@@ -5,52 +5,52 @@
 
 //Potentially remove fields from line 14-ish & 18-ish
 exports.login_check = function(encryptedEmail, encryptedPassword, callback) {
-	//console.log("email: " + encryptedEmail);
-	//console.log("password: " + encryptedPassword);
-	const connect_mysql = require("./connect_mysql");
-	let con = connect_mysql.connect();
-	con.connect(function(err) {
-		if (err) {throw err};
-		//console.log("Connected!");
-		con.query("SELECT Email FROM Account WHERE Email = ?", encryptedEmail , function (err, result, fields) {
-			if (err) {throw err};
-			//console.log(result.length);
-			if (result.length > 0){
-				let command = "SELECT Password AND Email FROM Account WHERE Password = ? AND Email = ?";
-				let values = [encryptedPassword,encryptedEmail];
-				con.query(command, values , function (err, result, fields) {
-					if(result.length > 0){
-						console.log("Login was successful - return login validation command");
-						callback(1);
-					}else{
-						console.log("Password is wrong failmessage"); 
-						callback(0);
-					}
-				});
-			}else {
-				console.log("Email does not exist - return wrong email infomation");
-				callback(0);
-			}
-		});
-	});
+  //console.log("email: " + encryptedEmail);
+  //console.log("password: " + encryptedPassword);
+  const connect_mysql = require("./connect_mysql");
+  let con = connect_mysql.connect();
+  con.connect(function(err) {
+    if (err) {throw err};
+    //console.log("Connected!");
+      con.query("SELECT Email FROM Account WHERE Email = ?", encryptedEmail , function (err, result, fields) {
+        if (err) {throw err};
+        //console.log(result.length);
+        if (result.length > 0){
+          let command = "SELECT Password AND Email FROM Account WHERE Password = ? AND Email = ?";
+          let values = [encryptedPassword,encryptedEmail];
+          con.query(command, values , function (err, result, fields) {
+            if(result.length > 0){
+              console.log("Login was successful - return login validation command");
+              callback(1);
+            }else{
+              console.log("Password is wrong failmessage"); 
+              callback(0);
+            }
+          });
+        }else{
+          console.log("Email does not exist - return wrong email infomation");
+          callback(0);
+        }
+      });
+    });
 }
 
 exports.email_check = function(email, callback) {
-	const connect_mysql = require("./connect_mysql");
-	let con = connect_mysql.connect();
-	con.connect(function(err) {
-		if (err) {throw err};
-		//console.log("Connected!");
-		con.query("SELECT Email FROM Account WHERE Email = ?", email , function (err, result, fields) {
-			if (err) {throw err};
-			//console.log(result.length);
-			if (result.length > 0){
-				//console.log("Email exists - successful login?");
-				callback(1);
-			}else {
-				//console.log("Email does not exist - return wrong email infomation");
-				callback(0);
-			}
-		});
-	});
+  const connect_mysql = require("./connect_mysql");
+  let con = connect_mysql.connect();
+  con.connect(function(err) {
+    if (err) {throw err};
+      //console.log("Connected!");
+    con.query("SELECT Email FROM Account WHERE Email = ?", email , function (err, result, fields) {
+      if (err) {throw err};
+      //console.log(result.length);
+      if (result.length > 0){
+        //console.log("Email exists - successful login?");
+        callback(1);
+      }else{
+        //console.log("Email does not exist - return wrong email infomation");
+        callback(0);
+      }
+    });
+  });
 }
